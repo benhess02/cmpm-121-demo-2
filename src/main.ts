090376edfbc76c171de87b009686b82b206b61cf
Ben Hess <pilotben@comcast.net>
2024-10-25 01:16:46 -0700

    Finished step 1: added non-interactive UI

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const titleText = document.createElement("h1");
+titleText.innerHTML = APP_NAME;
+app.append(titleText);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);

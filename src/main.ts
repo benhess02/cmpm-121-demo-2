4327ba6c03ca47159161797a6ce5b33d0d4e16f1
Ben Hess <pilotben@comcast.net>
2024-10-25 01:23:57 -0700

    Finished step 2: added simple marker drawing

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,39 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+const ctx = canvas.getContext("2d")!;
+
+const clearBtn = document.createElement("button");
+clearBtn.innerHTML = "Clear";
+app.append(clearBtn);
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
+
+canvas.addEventListener("mouseleave", (e) => {
+    cursor.active = false;
+});
+
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
